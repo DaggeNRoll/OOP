ca438c7562affcc21475a8dd8c5a63524e4e018a
Mikhail Trifonov <71384493+DaggeNRoll@users.noreply.github.com>
2021-04-19 12:44:51 +0300

    123

--- lab5/task6/cmake-build-debug/CMakeFiles/task6.dir/DependInfo.cmake
+++ lab5/task6/cmake-build-debug/CMakeFiles/task6.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/dagge/CLionProjects/OOP/lab5/task6/main.cpp" "C:/Users/dagge/CLionProjects/OOP/lab5/task6/cmake-build-debug/CMakeFiles/task6.dir/main.cpp.obj"
+  "D:/repos/OOP2/lab5/task6/main.cpp" "D:/repos/OOP2/lab5/task6/cmake-build-debug/CMakeFiles/task6.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
