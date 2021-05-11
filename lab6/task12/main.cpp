2eaf7b7b3860aa886a9ab2e7a008c784ea14130b
Mikhail Trifonov <71384493+DaggeNRoll@users.noreply.github.com>
2021-05-11 16:10:08 +0300

    6/12

---
+++ lab6/task12/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+class bMoney {
+    long double sum;
+    string ustring;
+public:
+    bMoney() {}
+
+    void mstold() {
+        string s1 = ustring;
+        s1.erase(0, 1);
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1[i] == ' ') {
+                s1.erase(i, 1);
+            }
+        }
+        sum = stold(s1);
+    }
+
+    void Idtoms() {
+        if (sum < 9999999999999990.00) {
+            ustring = to_string(sum);
+            for (int i = ustring.find('.') - 1, j = 0; i > -1; i--, j++) {
+                if (j % 3 == 0 && j != 0) {
+                    ustring.insert(i + 1, ",");
+                }
+            }
+            ustring.insert(0, "$");
+        } else {
+            cout << "Sum is too large!" << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    void madd(bMoney m1, bMoney m2) {
+        sum = m1.sum + m2.sum;
+    }
+
+    void stringOutput() const {
+        cout << "The answer is " << ustring << endl;
+    }
+
+    void getmoney() {
+        getline(cin, ustring);
+    }
+};
+
+int main() {
+    bMoney m1, m2, m3, m4;
+    cout << "Enter sum1" << endl;
+    m1.getmoney();
+    m1.mstold();
+    cout << "Enter sum2" << endl;
+    m2.getmoney();
+    m2.mstold();
+    cout << "Enter sum3" << endl;
+    m3.getmoney();
+    m3.mstold();
+    m4.madd(m1, m2);
+    m4.madd(m3, m4);
+    m4.Idtoms();
+    m4.stringOutput();
+
+}
--- lab6/task7/main.cpp
+++ lab6/task7/main.cpp
@@ -4,22 +4,24 @@
 
 using namespace std;
 
-class money{
+class money {
     long double sum;
 public:
-    money(){}
-    void mstold(string s){
+    money() {}
+
+    void mstold(string s) {
         string s1 = s;
-        s1.erase(0,1);
-        for (int i=0;i<s1.length();i++){
-            if (s1[i]==' '){
-                s1.erase(i,1);
+        s1.erase(0, 1);
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1[i] == ' ') {
+                s1.erase(i, 1);
             }
         }
-        sum=stold(s1);
+        sum = stold(s1);
     }
-    void moneyOutput()const{
-        cout << "Answer is " << setiosflags(ios::fixed) << setiosflags(ios::showpoint) << setprecision(2) <<  sum;
+
+    void moneyOutput() const {
+        cout << "Answer is " << setiosflags(ios::fixed) << setiosflags(ios::showpoint) << setprecision(2) << sum;
     }
 };
 
@@ -27,7 +29,7 @@ int main() {
     money m;
     cout << "Enter money:" << endl;
     string s;
-    getline(cin,s);
+    getline(cin, s);
     m.mstold(s);
     m.moneyOutput();
     return 0;
