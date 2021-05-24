31e1acf585c195fb3e6b89a4e150066226d68196
Mikhail Trifonov <71384493+DaggeNRoll@users.noreply.github.com>
2021-05-24 13:25:30 +0300

    7.6

--- lab7/task6/main.cpp
+++ lab7/task6/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -31,6 +32,10 @@ public:
 
     time operator--(int);
 
+    time operator-(time);
+
+    time operator*(float);
+
 };
 
 time time::operator+(time t2) {
@@ -57,6 +62,14 @@ time time::operator--(int) {
     return time(hours--, minutes--, seconds--);
 }
 
+time time::operator-(time t2) {
+    return time(hours - t2.hours, minutes - t2.minutes, seconds - t2.seconds);
+}
+
+time time::operator*(float d) {
+    return time(round(hours * d), round(minutes * d), round(seconds * d));
+}
+
 int main() {
     int h, m, s;
     char waste;
@@ -69,12 +82,34 @@ int main() {
     time t3 = t1 + t2;
     t3.output();
 
+    cout << "t1 = ";
+    t1.output();
+    cout << "t2 = ";
+    t2.output();
     t3 = ++t1 + t2++;
     cout << "++t1 + t2++ = ";
     t3.output();
 
+    cout << "t1 = ";
+    t1.output();
+    cout << "t2 = ";
+    t2.output();
     t3 = --t1 + t2--;
     cout << "--t1 + t2-- = ";
+    t3.output();
+
+    cout << "t1 = ";
+    t1.output();
+    cout << "t2 = ";
+    t2.output();
+    t3 = t2 - t1;
+    cout << "t2 - t1 = ";
+    t3.output();
+
+    cout << "t1 = ";
+    t1.output();
+    t3 = t1 * 1.5;
+    cout << "t1 * 1.5 = ";
     t3.output();
 
     return 0;
