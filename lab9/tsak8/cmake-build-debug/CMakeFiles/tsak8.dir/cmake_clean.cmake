28abda946d7ffa29f4afe93363b976ad09e3a5da
Mikhail Trifonov <71384493+DaggeNRoll@users.noreply.github.com>
2021-06-17 21:26:08 +0300

    9/8

---
+++ lab9/tsak8/cmake-build-debug/CMakeFiles/tsak8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tsak8.dir/main.cpp.obj"
+  "libtsak8.dll.a"
+  "tsak8.exe"
+  "tsak8.exe.manifest"
+  "tsak8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tsak8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
